a8c4e89d5751ae41137221a6785751c3a0a8fa55
Kamugisha Bruce <brucekamugisha36@gmail.com>
2023-04-12 09:04:35 +0300

    Adding tags to templating.

--- afro_dj/afro_dtl/views.py
+++ afro_dj/afro_dtl/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     pr_title = 'Afro-Django'
-    return render(request, 'index.html', {'pr_title': pr_title})
+    username = 'Mariam'
+    gender = 'Male'
+    return render(
+        request,
+        'index.html', 
+        {'pr_title': pr_title, 'username':username, 'gender':gender}
+    )
