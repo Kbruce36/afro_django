6ba06abed512799f0b5c25b1a70d2d01d17a45be
Kamugisha Bruce <brucekamugisha36@gmail.com>
2023-04-12 10:15:35 +0300

    Added register form

--- afro_dj/afro_dtl/views.py
+++ afro_dj/afro_dtl/views.py
@@ -4,9 +4,12 @@ from django.shortcuts import render
 def index(request):
     pr_title = 'Afro-Django'
     username = 'Mariam'
-    gender = 'Male'
+    gender = 'Female'
     return render(
         request,
         'index.html', 
         {'pr_title': pr_title, 'username':username, 'gender':gender}
     )
+
+def register(request):
+    return render(request, 'register.html')
