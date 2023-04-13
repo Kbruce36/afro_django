4c272b857dedd6201c85352e12f22a447958d8b6
Kamugisha Bruce <brucekamugisha36@gmail.com>
2023-04-13 09:51:17 +0300

    Added view to handle form data and  url

--- afro_dj/afro_dtl/views.py
+++ afro_dj/afro_dtl/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 def index(request):
     pr_title = 'Afro-Django'
@@ -12,4 +12,21 @@ def index(request):
     )
 
 def register(request):
+
     return render(request, 'register.html')
+
+def registration(request):
+    if request == 'POST':
+        name = request.POST.get['username']
+        email = request.POST.get['email']
+        password = request.POST.get['password']
+        gender = request.POST.get['gender']
+        user_details=[
+            name,email,password,gender
+        ]
+        print(user_details)
+    else:
+        pass
+
+
+   
