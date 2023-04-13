c27e29a7d0cfa7ffa305d1ae209a409b30ccf5b0
Kamugisha Bruce <brucekamugisha36@gmail.com>
2023-04-13 09:19:24 +0300

    Added custom model and gender field

--- afro_dj/afro_dtl/models.py
+++ afro_dj/afro_dtl/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+#we are creating  a custom user model "user acccount"
+class User_account(models.Model):
+    username = models.CharField(max_length=22 )
+    gender = models.CharField(max_length=6)
+    email = models.CharField(max_length=30)
+    password = models.CharField(max_length=22)
+    signup_time = models.DateTimeField(auto_now=True)
+    
+
